6cb59c7856093be20007e4cf1dae38ab07cef48e
klanguyen <nnguyen1@my.wctc.edu>
2020-01-31 16:33:43 -0600

    Create MovieApp database (script included)

---
+++ MovieAppDb_script.sql
@@ -0,0 +1,71 @@
+create schema kayla;
+
+create table movie
+(
+	movie_id int generated always as identity
+		constraint movie_pk
+			primary key,
+	movie_nm varchar(150) not null,
+	overview varchar(255)
+);
+
+create table genre
+(
+	genre_id int generated always as identity
+		constraint genre_pk
+			primary key,
+	description varchar(150) not null
+);
+
+create table movie_genre
+(
+	movie_id int
+		constraint movie_genre_MOVIE_MOVIE_ID_fk
+			references MOVIE,
+	genre_id int
+		constraint movie_genre_GENRE_GENRE_ID_fk
+			references GENRE,
+	constraint movie_genre_pk
+		primary key (movie_id, genre_id)
+);
+
+create table featuredcrew
+(
+	featuredcrew_id int generated always as identity
+		constraint featuredcrew_pk
+			primary key,
+	name varchar(150) not null
+);
+
+create table movie_featuredcrew
+(
+	movie_id int
+		constraint movie_featuredcrew_MOVIE_MOVIE_ID_fk
+			references MOVIE,
+	featuredcrew_id int
+		constraint movie_featuredcrew_FEATUREDCREW_FEATUREDCREW_ID_fk
+			references FEATUREDCREW,
+	constraint movie_featuredcrew_pk
+		primary key (movie_id, featuredcrew_id)
+);
+
+create table position
+(
+	position_id int generated always as identity
+		constraint position_pk
+			primary key,
+	description varchar(150) not null
+);
+
+create table featuredcrew_position
+(
+	featuredcrew_id int
+		constraint featuredcrew_position_FEATUREDCREW_FEATUREDCREW_ID_fk
+			references FEATUREDCREW,
+	position_id int
+		constraint featuredcrew_position_POSITION_POSITION_ID_fk
+			references POSITION,
+	constraint featuredcrew_position_pk
+		primary key (featuredcrew_id, position_id)
+);
+
