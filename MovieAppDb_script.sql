b802c76f21b12f10614a240349e1952fd5437333
klanguyen <nnguyen1@my.wctc.edu>
2020-05-25 23:21:33 -0500

    Add Database Tables

--- MovieAppDb_script.sql
+++ MovieAppDb_script.sql
@@ -71,3 +71,34 @@ create table featuredcrew_position
 
 alter table MOVIE
 	add column "Year" int;
+
+create table USERS(
+    username varchar(50) not null constraint user_pk primary key,
+    password varchar(60) not null,
+    enabled smallint default 1,
+    first_name varchar(50),
+    last_name varchar(50),
+    email varchar(50)
+);
+
+create table AUTHORITIES(
+    username varchar(50) not null references USERS(username),
+    authority varchar(50) not null
+);
+
+insert into USERS(username, password, enabled, first_name, last_name, email)
+values ('kayla', 'p', 1, 'Kayla', 'Nguyen', 'kayla@wctc.edu');
+
+insert into USERS(username, password, enabled, first_name, last_name, email)
+values ('hk', 'p', 0, 'Hello', 'Kitty', 'hkitty@wctc.edu');
+
+insert into USERS(username, password, first_name, last_name, email)
+values ('stacy', 'p', 'Stacy', 'Read', 'sread@wctc.edu');
+
+insert into AUTHORITIES values ('kayla', 'USER');
+insert into AUTHORITIES values ('hk', 'USER');
+insert into AUTHORITIES values ('stacy', 'USER');
+insert into AUTHORITIES values ('stacy', 'ADMIN');
+
+update USERS set password = '$2a$10$768dC3SVEGWYey9yaxmjZOPAJl3tSgXNEA.e5fLZybm0bjyOVLAF.'
+where password = 'p';
